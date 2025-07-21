33c4c06d208eeead073ac6ae0b1ab37b4fa84221
matthewjquigley <matthew.james.quigley@gmail.com>
2025-07-21 15:35:47 +1000

    #

--- WebApplication/Processing/CreateSVFAzure.cs
+++ WebApplication/Processing/CreateSVFAzure.cs
@@ -1,13 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Autodesk.Forge.DesignAutomation.Model;
-using Microsoft.Extensions.Logging;
 using WebApplication.Definitions;
 using WebApplication.Services;
-using WebApplication.Utilities;
 
 namespace WebApplication.Processing
 {
@@ -22,7 +17,6 @@ namespace WebApplication.Processing
         public override string Id => nameof(CreateSVFAzure);
         public override string Description => "Generate SVF and upload directly to Azure Blob Storage";
 
-        protected override string OutputUrl(ProcessingArgs projectData) => projectData.SvfUrl;
         protected override string OutputName => "SvfOutput";
         protected override bool IsOutputZip => true;
 
@@ -31,88 +25,49 @@ namespace WebApplication.Processing
             _azureBlobService = azureBlobService;
         }
 
-        protected override async Task<List<Autodesk.Forge.DesignAutomation.Model.Argument>> BuildArgumentsAsync(ProcessingArgs data)
+        public override Dictionary<string, IArgument> ToWorkItemArgs(ProcessingArgs data)
         {
-            var arguments = await base.BuildArgumentsAsync(data);
+            // Get base arguments
+            var args = base.ToWorkItemArgs(data);
             
-            // Add onDemand argument for direct Azure upload
-            if (await _azureBlobService.IsConfiguredAsync())
+            // Add Azure output argument if Azure is configured
+            if (_azureBlobService.IsConfiguredAsync().GetAwaiter().GetResult())
             {
-                var azureOutputArg = await CreateAzureOutputArgumentAsync(data);
-                arguments.Add(azureOutputArg);
-            }
-            
-            return arguments;
-        }
-
-        private async Task<Autodesk.Forge.DesignAutomation.Model.Argument> CreateAzureOutputArgumentAsync(ProcessingArgs data)
-        {
-            // Generate Azure blob path
-            var blobPath = $"svf/{data.ProjectId}/{data.ParametersHash}/SvfOutput.zip";
-            
-            // Generate SAS URL with write permissions
-            var sasUrl = await _azureBlobService.GenerateSasUrlAsync(
-                blobPath, 
-                Azure.Storage.Sas.BlobSasPermissions.Write | Azure.Storage.Sas.BlobSasPermissions.Create,
-                expirationMinutes: 60
-            );
-
-            // Create onDemand output argument
-            return new Autodesk.Forge.DesignAutomation.Model.Argument
-            {
-                Url = sasUrl,
-                Verb = Autodesk.Forge.DesignAutomation.Model.Argument.VerbEnum.Put,
-                LocalName = "SvfOutputAzure",
-                Required = false,
-                Headers = new Dictionary<string, string>
-                {
-                    { "x-ms-blob-type", "BlockBlob" }
-                }
-            };
-        }
-
-        protected override async Task<WorkItemStatus> CreateWorkItemAsync(ProcessingArgs projectData)
-        {
-            var workItem = await base.CreateWorkItemAsync(projectData);
-            
-            // Store Azure blob path in project data for later retrieval
-            if (await _azureBlobService.IsConfiguredAsync())
-            {
-                var blobPath = $"svf/{projectData.ProjectId}/{projectData.ParametersHash}/SvfOutput.zip";
-                projectData.SetAzureSvfBlobPath(blobPath);
-            }
-            
-            return workItem;
-        }
-
-        protected override async Task OnSuccessAsync(ProcessingArgs data, WorkItemStatus workItemStatus)
-        {
-            await base.OnSuccessAsync(data, workItemStatus);
-            
-            // If Azure upload was successful, update the SVF URL to point to Azure
-            if (!string.IsNullOrEmpty(data.GetAzureSvfBlobPath()))
-            {
-                var blobPath = data.GetAzureSvfBlobPath();
-                var azureUrl = await _azureBlobService.GetModelUrlAsync(blobPath);
-                data.SvfUrl = azureUrl;
+                // Generate a unique blob path based on timestamp
+                var timestamp = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+                var blobPath = $"svf/output_{timestamp}/SvfOutput.zip";
                 
-                Logger.LogInformation($"SVF successfully uploaded to Azure: {blobPath}");
-            }
-        }
-    }
+                // Generate SAS URL with write permissions
+                var sasUrl = _azureBlobService.GenerateSasUrlAsync(
+                    blobPath, 
+                    Azure.Storage.Sas.BlobSasPermissions.Write | Azure.Storage.Sas.BlobSasPermissions.Create,
+                    expirationMinutes: 60
+                ).GetAwaiter().GetResult();
 
-    public static class ProcessingArgsExtensions
-    {
-        private const string AzureSvfBlobPathKey = "AzureSvfBlobPath";
-        
-        public static string GetAzureSvfBlobPath(this ProcessingArgs args)
-        {
-            return args.TryGetValue(AzureSvfBlobPathKey, out var value) ? value as string : null;
+                // Add onDemand output argument for direct Azure upload
+                args["AzureOutput"] = new XrefTreeArgument
+                {
+                    Url = sasUrl,
+                    Verb = Verb.Put,
+                    LocalName = "SvfOutputAzure",
+                    Optional = true,
+                    Headers = new Dictionary<string, string>
+                    {
+                        { "x-ms-blob-type", "BlockBlob" }
+                    }
+                };
+                
+                // Store the Azure URL in the output URL for later retrieval
+                data.SvfUrl = sasUrl;
+            }
+            
+            return args;
         }
-        
-        public static void SetAzureSvfBlobPath(this ProcessingArgs args, string value)
+
+        protected override string OutputUrl(ProcessingArgs data)
         {
-            args[AzureSvfBlobPathKey] = value;
+            // Return the stored SVF URL (which might be Azure URL if configured)
+            return data.SvfUrl ?? base.OutputUrl(data);
         }
     }
 }
